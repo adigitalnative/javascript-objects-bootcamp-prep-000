686e1dc4e2a954506e16c0f6a712d0b8bb1b0178
adigitalnative@gmail.com <adigitalnative@gmail.com>
2018-08-23 02:44:54 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,3 +4,7 @@ function updatePlaylist(playlist, name, title) {
   playlist = Object.assign(playlist, {[name]: title});
   return playlist
 }
+
+function removeFromPlaylist(playlist, name) {
+  delete playlist[name]
+}
